904d8d1fcb2c2005ad4806fbeca27fca8e4cb33b
Fran Glez <fcoj.glez94@gmail.com>
2024-07-12 21:45:06 +0200

    Added votacion page and updated other files.
    Updated pages:
    - Added new page: /votacion
    - Updated layout: added title to resultados page
    - Updated style: changed max-width of svg to 100%
    
    Updated store:
    - Added vote storage using zustand
    - Updated theme storage using zustand

--- src/components/FormularioApuestada.tsx
+++ src/components/FormularioApuestada.tsx
@@ -1,52 +1,149 @@
-import { useState } from "react";
-import { Col, Form, Row, Image, Button } from "react-bootstrap";
+import { Col, Form, Row, Image, Button, Alert } from "react-bootstrap";
 
 import combats from "../assets/combats.json";
+import { classNames } from "../util";
+import { useVotesStore, type Vote } from "../store/votes";
+import { useState, type FormEvent } from "react";
 
 export default function FormularioApuestada() {
+  const [error, setError] = useState<Error>();
+  const addVote = useVotesStore((d) => d.addVote);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = Array.from(new FormData(e.currentTarget));
+    const userName = String(data[0][1]);
+    const voteMatchArray = data.slice(1);
+    try {
+      console.log(userName, voteMatchArray);
+      const vote: Vote = voteMatchArray.reduce<Vote>(
+        (pre, [match, team]) => ({ ...pre, [match]: team as string }),
+        {}
+      );
+      console.log(vote);
+      addVote(userName, vote);
+    } catch (error) {
+      if (error instanceof Error) setError(error);
+      else setError(new Error("Error desconocido"));
+    }
+    e.currentTarget.reset();
+  };
+  if (error)
+    return (
+      <Alert variant="danger" dismissible onClose={() => setError(undefined)}>
+        <Alert.Heading>Error</Alert.Heading>
+        <div>{error.message}</div>
+      </Alert>
+    );
+
   return (
-    <Form className="m-5">
-      <h2>Apuestador</h2>
-      <Form.Group as={Row} className="my-3 ">
-        <Form.Label column sm="2">
-          Nombre
+    <Form className="my-5" onSubmit={handleSubmit}>
+      <Form.Group as={Row} className="mb-3 text-start">
+        <Form.Label className="fs-2 mb-3" column sm="6">
+          Nombre de colegón:
         </Form.Label>
-        <Col sm="10">
-          <Form.Control defaultValue="gonpardo" />
+        <Col sm="6" className="mb-3">
+          <Form.Control
+            name="user"
+            className="fs-2"
+            placeholder="gonpardo"
+            required
+            minLength={6}
+            maxLength={20}
+          />
         </Col>
       </Form.Group>
-      <h2>Combates</h2>
-
-      {combats.map((combat, combatIdx) => (
-        <Form.Group as={Row} className="mb-3" key={combatIdx}>
-          <Col sm="12">
-            <div className="d-flex justify-content-center align-items-center flex-wrap">
-              {/*Imagenes de los combatientes*/}
-              <Image src={combat.photos[0]} alt={combat.teams[0].join(" & ")} width={100} className="m-2" />
-              <Image src={combat.banner} alt={combat.name} width={200} className="m-2" />
-              <Image src={combat.photos[1]} alt={combat.teams[1].join(" & ")} width={100} className="m-2" />
-            </div>
-          </Col>
-          <Col sm="12">
-            {/*Flex y justify content pa centrar las votaciones*/}
-            <Form.Group className="mt-3 px-3 d-flex justify-content-center align-items-center flex-wrap">
-              {combat.teams.map((team, teamIdx) => (
-                <Form.Check
-                  key={teamIdx}
-                  type="radio"
-                  name={"combat-" + combatIdx}
-                  label={team.join(" & ").toLocaleUpperCase()}
-                  value={teamIdx}
-                />
-              ))}
-            </Form.Group>
-          </Col>
-          <hr className="my-3" />
+      <section>
+        <h2>Combates</h2>
+        <Form.Group className="m-3 rounded">
+          {combats.map((combat, combatIdx) => (
+            <Combat key={combat.name} {...{ combatIdx, combat }} />
+          ))}
         </Form.Group>
-      ))}
+      </section>
       <Button className="w-100" type="submit">
         Enviar
       </Button>
     </Form>
+  );
+}
+
+type CombatProps = {
+  combat: {
+    name: string;
+    teams: {
+      name: string;
+      photo?: string;
+    }[];
+    banner: string;
+  };
+};
+
+function Combat({ combat }: CombatProps) {
+  return (
+    <Form.Group as={Row} className="mb-3 border-top border-1 border-primary">
+      <Col sm="12">
+        <Row className="justify-content-md-center p-3">
+          <Image src={combat.banner} alt={combat.name} />
+        </Row>
+        <Row className="d-flex align-items-center">
+          {combat.teams.map(
+            (team, teamIdx) =>
+              team.photo && (
+                <Col key={`${teamIdx}-${team.name}`} xs={6}>
+                  <Image
+                    src={team.photo}
+                    height={200}
+                    alt={team.name}
+                    style={{
+                      maskImage: "linear-gradient(black 90%, transparent 100%)",
+                    }}
+                  />
+                </Col>
+              )
+          )}
+        </Row>
+      </Col>
+      <Col sm="12">
+        {/*Flex y justify content pa centrar las votaciones*/}
+        <Form.Group
+          as={Row}
+          style={{ maxWidth: "40em" }}
+          className="m-3 d-flex justify-content-center flex-wrap mx-auto"
+        >
+          {combat.teams.map((team, teamIdx) => (
+            <Col xs={6} md={6} key={teamIdx}>
+              <TeamCheckBox
+                {...{
+                  combatName: combat.name,
+                  team,
+                  reverse: teamIdx % 2 !== 0,
+                }}
+              />
+            </Col>
+          ))}
+        </Form.Group>
+      </Col>
+    </Form.Group>
+  );
+}
+
+interface TeamCheckBoxProps {
+  reverse: boolean;
+  combatName: string;
+  team: { name: string; photo?: string };
+}
+
+function TeamCheckBox({ combatName, team, reverse = true }: TeamCheckBoxProps) {
+  return (
+    <Form.Check
+      className={classNames("text-center", "me-auto")}
+      reverse={reverse}
+      type="radio"
+      name={combatName}
+      label={team.name.toUpperCase()}
+      value={team.name}
+      required
+    />
   );
 }
---
+++ src/store/votes.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type UserName = string;
+type TeamName = string;
+type MatchName = string;
+
+export type Vote = {
+  [key in MatchName]: TeamName;
+};
+
+interface UserVote {
+  user: UserName;
+  vote: Vote;
+}
+
+interface VotesState {
+  votes: UserVote[];
+  addVote(user: UserName, vote: Vote): void;
+  deleteAllVotes(): void;
+}
+
+export const useVotesStore = create<VotesState>()(
+  persist(
+    (set, get) => ({
+      votes: [],
+      addVote: (user, vote) => {
+        const existingVote = get().votes.find((v) => v.user === user);
+        if (existingVote) throw Error("User already voted");
+        const name = user.trim().replace(/\s/g, "");
+        if (!name || !name.length || name.length < 3)
+          throw Error("Invalid user name");
+        set({ votes: [...get().votes, { user, vote }] });
+      },
+      deleteAllVotes: () => {
+        set({ votes: [] });
+      },
+    }),
+    {
+      name: "vote-storage",
+    }
+  )
+);
